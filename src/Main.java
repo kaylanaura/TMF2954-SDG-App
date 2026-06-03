d04a1c5653ac564009fa94dbd80ea38eb9913149
kaylanaura <kaylachemad@gmail.com>
2026-06-03 17:58:25 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 // Class      : Main
-// Creator    : [Member 4 Name]  ← Member 4 owns this file
-// Tester     : [Member 1 Name]
+// Creator    : Kayla binti Mohamad 102641
+// Tester     : Victoria Ngui Fong Eik 106647
 // Description: Entry point for the SDG 3 Health & Well-Being desktop app.
 //              Sets up the main JFrame with smartphone-sized dimensions
 //              and launches the Learning Module.
