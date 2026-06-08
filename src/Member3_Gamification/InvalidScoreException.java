c213ebb7379f58e72693388fdc02d007da651bb3
izwanO <izwanomar3@gmail.com>
2026-06-09 00:29:51 +0800

    Update InvalidScoreException.java

--- src/Member3_Gamification/InvalidScoreException.java
+++ src/Member3_Gamification/InvalidScoreException.java
@@ -3,7 +3,6 @@
 // Tester     : Kayla Binti Mohamad (102641)
 // Description: Custom exception for invalid quiz percentage.
 //
-// Lecture Concept Applied:
 // Custom Exception:
 // - This class extends the predefined Exception class.
 // - It is used to represent an unusual situation where the percentage is below 0 or above 100.
